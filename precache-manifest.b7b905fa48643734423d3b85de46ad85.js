84482b3edf7c3e631a16b8c6e6b0f4b6640d3d18
asishkr <ashish.kumar@vidyalai.com>
2020-05-06 11:49:45 +0530

    Updates

---
+++ precache-manifest.b7b905fa48643734423d3b85de46ad85.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fb8705e3fc53297cabc0262d64f2a9",
+    "url": "/developer-profile/index.html"
+  },
+  {
+    "revision": "cc316f6c68df41563c34",
+    "url": "/developer-profile/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6ff6b561ac5f813a8802",
+    "url": "/developer-profile/static/css/main.147ae8f1.chunk.css"
+  },
+  {
+    "revision": "cc316f6c68df41563c34",
+    "url": "/developer-profile/static/js/2.4b31819a.chunk.js"
+  },
+  {
+    "revision": "6ff6b561ac5f813a8802",
+    "url": "/developer-profile/static/js/main.ea311288.chunk.js"
+  },
+  {
+    "revision": "2024bbd7071e3c938f25",
+    "url": "/developer-profile/static/js/runtime~main.f5722bca.js"
+  },
+  {
+    "revision": "20c39e408ebb0ded0a3defa5f8498068",
+    "url": "/developer-profile/static/media/background.20c39e40.png"
+  },
+  {
+    "revision": "9ff53127ba741e015ae1ea7c384c2b51",
+    "url": "/developer-profile/static/media/giphy.9ff53127.gif"
+  },
+  {
+    "revision": "918182732336420576c56b80369ce749",
+    "url": "/developer-profile/static/media/me.91818273.jpg"
+  },
+  {
+    "revision": "9dd0570d8b4d7703d4bac6337572a2d6",
+    "url": "/developer-profile/static/media/pattern.9dd0570d.png"
+  }
+]);
