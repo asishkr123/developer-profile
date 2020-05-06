7cd7e6a0317be29b73f5e5b9a3a0cb74c79f4e78
asishkr <ashish.kumar@vidyalai.com>
2020-05-06 11:42:22 +0530

    third commit

--- src/Skill/SkillList.js
+++ src/Skill/SkillList.js
@@ -1,5 +1,12 @@
-export const frontEnd = [ 'HTML', 'CSS', 'JavaScript ES6', 'Flutter', 'React.js', 'Bootstrap' ];
+export const frontEnd = [
+  "HTML",
+  "CSS",
+  "JavaScript ES6",
+  "Flutter",
+  "React.js",
+  "Bootstrap"
+];
 
-export const backEnd = [ 'Node.js', 'MySQL', 'Sequelize.js', 'MongoDB', 'Mongoose.js', 'Django' ];
+export const backEnd = ["Node.js", "MongoDB", "Mongoose.js", "Django"];
 
-export const other = [ 'GIT', 'RESTful APIs', 'Mobile Design', 'Dart', 'Java Android','Adobe Dreamweaver','Design Wireframe' ];
+export const other = ["GIT", "RESTful APIs", "Dart"];
