1cab8c6463f2cfd2eec6009a4362f4ca0d726041
asishkr <ashish.kumar@vidyalai.com>
2020-05-06 11:41:06 +0530

    second commit

--- src/About/EducationTab.js
+++ src/About/EducationTab.js
@@ -1,40 +1,16 @@
 const EducationTabs = [
-	{
-		name: 'Sindhi Model SS School',
-		done: '10th Grade',
-		year: '2014',
-		type: 'https://img.icons8.com/color/48/000000/school.png'
-    },
-    {
-		name: 'Maharishi Vidya Mandir',
-		done: '12th Grade',
-		year: '2014-2016',
-		type: 'https://img.icons8.com/color/48/000000/school.png'
-    },
-    {
-		name: 'SRM Institue of Science and Technology',
-		done: 'BTech - Computer Science',
-		year: '2016-2020',
-		type: 'https://img.icons8.com/color/48/000000/student-male.png'
-    },
-    {
-		name: 'Big Daddy',
-		done: 'E-commerce Startup',
-		year: '2018',
-		type: 'https://img.icons8.com/color/48/000000/hamburger.png'
-    },
-    {
-		name: 'Uniq Technologies',
-		done: 'Android Development Internship',
-		year: '2018',
-		type: 'https://img.icons8.com/color/48/000000/laptop.png'
-    },
-    {
-		name: 'NIIT Chennai',
-		done: 'Web Development - Java ',
-		year: '2017-2018',
-		type: 'https://img.icons8.com/color/48/000000/laptop.png'
-    }
+  {
+    name: "St.mary's english high school",
+    done: "10th Grade",
+    year: "2011",
+    type: "https://img.icons8.com/color/48/000000/school.png"
+  },
+  {
+    name: "St.mary's english high school",
+    done: "12th Grade",
+    year: "2011-2013",
+    type: "https://img.icons8.com/color/48/000000/school.png"
+  }
 ];
 
 export default EducationTabs;
--- src/About/LeftPhoto.js
+++ src/About/LeftPhoto.js
@@ -1,52 +1,55 @@
-import React from 'react';
-import Tilt from 'react-tilt';
+import React from "react";
+import Tilt from "react-tilt";
 
-import ContactIcon from './ContactIcon';
-import Me from './me.jpg';
-import instagram from './instagram.png';
-import whatsapp from './whatsapp.png';
-import gmail from './gmail.png';
+import ContactIcon from "./ContactIcon";
+import Me from "./me.jpg";
+import instagram from "./instagram.png";
+import whatsapp from "./whatsapp.png";
+import gmail from "./gmail.png";
 
 const LeftPhoto = () => {
-	return (
-		<div className="ph3 pt3 ml5-l">
-			<div className="tc">
-				<div className="hide-child relative b--black-20 mw5 center">
-					<Tilt className="Tilt" options={{ max: 25 }}>
-						<div className="Tilt-inner">
-							<img
-								className="br4 shadow-5"
-								style={{
-									height: '345px'
-								}}
-								alt="Siddhant Bajoria"
-								src={Me}
-							/>
-						</div>
-					</Tilt>
-					<div className="bt b--black-20">
-						<p className="f4 fw7 ttu animated pulse infinite">Siddhant Bajoria</p>
-					</div>
-					<small>Contact Me</small>
-					<hr className="mw3 bb bw1 b--black-10" />
-					<div className="flex flex-wrap justify-center">
-						<ContactIcon
-							icon={whatsapp}
-							size="40"
-							link="https://wa.me/07338766426/?text=Hello%20there%20!"
-						/>
-						<ContactIcon
-							icon={instagram}
-							istyle="mt2"
-							size="30"
-							link="https://www.instagram.com/sid1n_only/?hl=en"
-						/>
-						<ContactIcon icon={gmail} size="40" link="mailto:siddhanthbajoria@gmail.com?Subject=Hello%20" />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="ph3 pt3 ml5-l">
+      <div className="tc">
+        <div className="hide-child relative b--black-20 mw5 center">
+          <Tilt className="Tilt" options={{ max: 25 }}>
+            <div className="Tilt-inner">
+              <img
+                className="br4 shadow-5"
+                style={{
+                  height: "345px"
+                }}
+                alt="Asish Kumar"
+              />
+            </div>
+          </Tilt>
+          <div className="bt b--black-20">
+            <p className="f4 fw7 ttu animated pulse infinite">Asish Kumar</p>
+          </div>
+          <small>Contact Me</small>
+          <hr className="mw3 bb bw1 b--black-10" />
+          <div className="flex flex-wrap justify-center">
+            <ContactIcon
+              icon={whatsapp}
+              size="40"
+              link="https://wa.me/07079021560/?text=Hello%20there%20!"
+            />
+            <ContactIcon
+              icon={instagram}
+              istyle="mt2"
+              size="30"
+              link="https://www.instagram.com/asishkr9244/?hl=en"
+            />
+            <ContactIcon
+              icon={gmail}
+              size="40"
+              link="mailto:asishkr2794@gmail.com?Subject=Hello%20"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftPhoto;
