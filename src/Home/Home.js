24754fc3937e5311fd012870cb77c50ef12e1d90
asishkr <ashish.kumar@vidyalai.com>
2020-05-06 11:54:56 +0530

    remove wrong name

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import giphy from './giphy.gif';
+import React from "react";
+import giphy from "./giphy.gif";
 const Home = () => {
-	return (
-		<div>
-			<p className="tc fw6 f-subheadline lh-solid ttu tracked-tight red">
-				Siddhant <span className="yellow">Bajoria</span>
-			</p>
+  return (
+    <div>
+      <p className="tc fw6 f-subheadline lh-solid ttu tracked-tight red">
+        Asish <span className="yellow">Kumar</span>
+      </p>
 
-			<p className="tc f1 fw5 lh-solid ttu light-red animated bounceInLeft">Developer</p>
-			<div className="tc">
-				<img src={giphy} alt="as" title="presentation" frameBorder="0" />
-			</div>
-		</div>
-	);
+      <p className="tc f1 fw5 lh-solid ttu light-red animated bounceInLeft">
+        Developer
+      </p>
+      <div className="tc">
+        <img src={giphy} alt="as" title="presentation" frameBorder="0" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
